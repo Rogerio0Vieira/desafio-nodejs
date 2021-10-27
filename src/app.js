af517e542e0a03156fde5e8fbb2863b3966aff72
Rogerio Vieira <rogeriovbh@gmail.com>
2021-10-26 21:30:14 -0300

    put, delete criados

--- src/app.js
+++ src/app.js
@@ -32,11 +32,50 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+
+  const {title, url, techs} = request.body
+
+  const {id} = request.params;
+
+  const findRepositoryIndex = repositories.findIndex(repository=>
+    repository.id === id
+    );
+
+    if (findRepositoryIndex === -1){
+      return response.status(400).json({error: 'Repository dos not exists.'})
+    }
+
+    const repository ={
+      
+        id,
+        title,
+        url,
+        techs,
+        likes: repositories[findRepositoryIndex].likes,
+      };
+    
+
+    repositories[findRepositoryIndex] = repository
+
+    return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const findRepositoryIndex = repositories.findIndex(repository=>
+    repository.id === id
+    );
+
+    if(findRepositoryIndex => 0){
+      repositories.splice(findRepositoryIndex, 1);
+    }else{
+
+      return response.status(400).json({error: 'Repository does not exists.'});
+
+    }
+
+    return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
